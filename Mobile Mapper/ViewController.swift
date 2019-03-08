d925a675dcd7f6e588acb8a64d4d0e7d54488134
Dylan Keenan <dylankeenan&icloud.com>
2019-03-08 11:52:16 -0600

    Finished Step 3

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
-
+import MapKit
+import CoreLocation
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let barringtonAnnotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+        let latitude = 42.15704
+        let longitude = -88.14812
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        barringtonAnnotation.title = "Barrington High School"
+        mapView.addAnnotation(barringtonAnnotation)
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for placemark in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+            
+        }
+        
+        
 }
-
+}
